8f26744fec784a707fcef38b073961bf931ef371
sarthaksahni1 <46090964+sarthaksahni1@users.noreply.github.com>
2018-12-24 13:15:44 +0530

    Delete download.sh

--- Malaria-Detection/datasets/download.sh
+++
@@ -1,4 +0,0 @@
-echo "Getting the Malaria Dataset"
-echo "Starting Download..."
-wget --no-check-certificate https://www.kaggle.com/iarunava/cell-images-for-detecting-malaria/downloads/cell-images-for-detecting-malaria.zip/1
-echo "Dataset Downloaded Successfully!!"
